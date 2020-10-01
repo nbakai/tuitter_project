76e1c63747322b9b624db70c08c608401fbce7db
Melisa Budini <melixbq@gmail.com>
2020-09-30 22:24:50 -0300

    Update: views index and show now with picture

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,5 @@
 class Tweet < ApplicationRecord
     belongs_to :user
     has_many :likes, dependent: :destroy
+    validates :content, length: {maximum: 140, message: 'El máximo es de 140 caracteres!'}
 end
