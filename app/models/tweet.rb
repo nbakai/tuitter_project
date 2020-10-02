4b400884ffab34d6ca06914120369cb3b31c5fce
Melisa Budini <melixbq@gmail.com>
2020-10-02 12:17:06 -0300

    Add: scope my tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,4 +6,6 @@ class Tweet < ApplicationRecord
     validates :content, length: {minimum: 2, message: 'Debes colocarle contenido al tweet mín 2 caracteres!'}
     validates :content, length: {maximum: 140, message: 'El máximo es de 140 caracteres!'}
     validates :content, presence: true, allow_blank: false 
+
+    scope :my_tweets, -> (current_user) {where(user_id: current_user)}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_013623) do
+ActiveRecord::Schema.define(version: 2020_10_02_144752) do
+
+  create_table "friends", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friends_on_friend_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "tweet_id"
