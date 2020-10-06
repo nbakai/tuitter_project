ab6026d4b739fa18becd4889dbea3aacf2c05571
Melisa Budini <melixbq@gmail.com>
2020-10-06 00:17:07 -0300

    up

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
     end
   end
   
-  resources :friends
+  resources :friends, only: [:create, :destroy]
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tweets#index'
