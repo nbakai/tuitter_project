c5189e1bb7e3edeffc627d3af879376a3e123357
Melisa Budini <melixbq@gmail.com>
2020-10-09 09:24:16 -0300

    notf

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tweets, :dependent => :destroy
   has_many :likes, dependent: :destroy
-  has_many :active_friends, class_name:  "Friend", foreign_key: "friend_id", dependent: :destroy
+  has_many :friends, class_name:  "Friend", foreign_key: "friend_id", dependent: :destroy
   has_many :no_friends, class_name: "Friend", foreign_key: "user_id", dependent: :destroy
  
-  has_many :following, through: :active_friends, source: :user
+  has_many :following, through: :friends, source: :user
   has_many :followers, through: :no_friends, source: :friend
 
 
@@ -17,12 +17,12 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   def follow(other_user)
-    active_friends.create(user_id: other_user.id)
+    friends.create(user_id: other_user.id)
   end
 
   
   def unfollow(other_user)
-    active_friends.find_by(user_id: other_user.id).destroy
+    friends.find_by(user_id: other_user.id).destroy
   end
   def following?(other_user)
     following.include?(other_user)
